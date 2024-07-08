8e608a0f50eaa74f7050653d8e9145cbb7ed71a6
pipon7 <marcosxd20022018@hotmail.com>
2024-07-08 20:50:53 -0300

    actualiza

--- app.js
+++ app.js
@@ -1,13 +1,36 @@
 // document.querySelector() trae una variable de otro archivo. Se le agrega un let para poder meterlo en una caja dentro de nuestro js y modificarlo.
 // titulo.innerhtml = titulo es el nombre de la caja creada e innerhtml se refiere al tipo de documento que seleccionamos.
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Juego del numero secreto'
+/* ejemplo del fuction de asignarTextoElemento pero NO reducido:
+ let parrafo = document.querySelectorAll('p');
+parrafo.innerHTML = 'Indica un numero del 1 al 10.'; */
 
-// Repito
-let parrafo = document.querySelector('p');
-parrafo.innerHTML = 'Indica un mumero del 1 al 10.'
+//Variables.
+let numeroSecreto = generarNumeroSecreto(); //Esta variable funciona ya que los fuctions no importan en que orden del codigo esten.
+console.log(numeroSecreto);
 
 // function = hacer una accion
-function intentoDeUsuario() {
-    alert('Click desde el botón.')
+function verificarIntento() {
+    let numeroDeUsuario = document.getElementById('valorUsuario').value; //getelementbyid para seleccionar un ID de html y el .value es para tomar el valor ingresado
+    console.log(numeroDeUsuario);
+    return;
 }
+
+/* En este caso fuction crea la variable asignarTextoElemento 
+para que esta pueda ser reutilizada cada vez que querramos, en los
+() se crean dos cajas llamadas elemento y texto las cuales abajo
+ con el document.queryselector y el elemnto.innerhtml hacen que
+ cada vez que se utilice la variable asignarTextoElemento, dentro
+ de los () solo debamos agregar el document.queryselector que 
+ querramos seleccionar y el inner.html en forma de texto que querramos */
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+
+function generarNumeroSecreto() {
+    return Math.floor(Math.random()*10) + 1;
+}
+
+asignarTextoElemento('h1', 'Juego del numero secreto');
+asignarTextoElemento('p', 'Indica un numero del 1 al 10.');
