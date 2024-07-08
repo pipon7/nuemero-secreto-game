f24372dec2714edbad3e8a40cf8d24e2d4179eb0
pipon7 <marcosxd20022018@hotmail.com>
2024-07-08 19:55:56 -0300

    actualiza

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-let titulo = document.querySelector('h1')
+// document.querySelector() trae una variable de otro archivo. Se le agrega un let para poder meterlo en una caja dentro de nuestro js y modificarlo.
+// titulo.innerhtml = titulo es el nombre de la caja creada e innerhtml se refiere al tipo de documento que seleccionamos.
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Juego del numero secreto'
+
+// Repito
+let parrafo = document.querySelector('p');
+parrafo.innerHTML = 'Indica un mumero del 1 al 10.'
+
+// function = hacer una accion
+function intentoDeUsuario() {
+    alert('Click desde el botón.')
+}
