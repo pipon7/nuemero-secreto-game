5942dd0b82ac2c659cc3f48f416cf427afbf028a
pipon7 <marcosxd20022018@hotmail.com>
2024-07-20 23:28:12 -0300

    actualiza

--- desafio4.js
+++ desafio4.js
@@ -53,3 +53,34 @@ function sumaTotal(listita) {
 let numerosSumar = [90, 70, 100, 150, 700, 57, 66];
 let resultado = sumaTotal(numerosSumar);
 console.log(`El resultado de la suma de la lista es ${resultado}`);
+// 9
+function verificar(numbers, elemento) {
+    console.log(numbers.indexOf(elemento));
+}
+let listaNumbers = [80, 70, 55, 97, 60, 40];
+let ver = verificar(listaNumbers, 99);
+// 10
+function dosListas(listaUno, listaDos) {
+    let sumaUno = 0;
+    let sumaDos = 0;
+    for(var i = 0; i < listaUno.length; i++) {
+        sumaUno += listaUno[i];
+    }
+    for(var j = 0; j < listaDos.length; j++) {
+    sumaDos += listaDos[j];
+    }
+    return sumaUno + sumaDos;
+}
+let primerLista = [50, 90, 500, 700, 55, 41];
+let segundaLista = [80, 50, 11, 36, 87, 300];
+let sumaDeListas = dosListas(primerLista, segundaLista);
+console.log(`El resultado de las dos listas es ${sumaDeListas}`);
+// 11
+function cuadradoDeNumeros(listaCuadrada) {
+    return listaCuadrada.map(function(numero) {
+        return numero * numero;
+    });
+}
+let numerosCuadrados = [1, 2, 3, 4, 5, 7, 21];
+let cuadrados = cuadradoDeNumeros(numerosCuadrados);
+console.log(`La lista de los cuadrados es: ${cuadrados}`);
